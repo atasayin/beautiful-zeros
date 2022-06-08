fe1ac400f646ad98304a047bf4abb7ac8797c886
Onur Eren Arpaci <onurerenarpaci@hotmail.com>
2022-06-08 20:53:31 +0300

    added the option to see the solution

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 n, k = [int(x) for x in input().split()]
 
@@ -6,6 +7,8 @@ Cost = [int(x) for x in input().split()]
 first_idx = (n - k -1) % (2*k + 1)
 
 result = None
+sol_idx = None
+Solution = []
 
 for i in range(first_idx, k+1):
     cost_i = 0
@@ -13,7 +16,16 @@ for i in range(first_idx, k+1):
         cost_i = cost_i + Cost[j]
     if result is None:
         result = cost_i
+        sol_idx = i
     else:
-        result = min(result, cost_i) 
+        result = min(result, cost_i)
+        if result == cost_i:
+            sol_idx = i 
 
 print(result)
+
+# check if there are arguments
+if len(sys.argv) > 1:
+    for j in range(sol_idx, n, 2*k+1):
+        Solution.append(j)
+    print(Solution)
